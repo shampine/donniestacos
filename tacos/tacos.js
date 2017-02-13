ad00286f215615dddce0dffcace3e7894ccbae1d
patrickshampine <patrick@patrickshampine.com>
2017-02-13 00:06:55 -0800

    Added initial list of food

---
+++ tacos/tacos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  "taco",
+  "burrito",
+  "guacomole",
+  "salsa",
+  "tamale",
+  "tostada",
+  "enchilada",
+  "quesadilla",
+  "nachos",
+  "pico de gallo",
+  "chalupa",
+  "churro"
+];
